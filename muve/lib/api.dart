00c32114680d6a3f9bd475f647fe49dd6730177c
Cleiton-Martins <161392160+Cleiton-Martins@users.noreply.github.com>
2026-09-18 19:08:11 -0300

    inserir link do Vercel

--- muve/lib/api.dart
+++ muve/lib/api.dart
@@ -1,4 +1,3 @@
 class ApiConfig {
-  // Emulador Android → localhost do host é 10.0.2.2
-  static const baseUrl = 'http://10.0.2.2:3000';
+  static const String baseUrl = 'https://dev-ops-eight-bay.vercel.app';
 }
